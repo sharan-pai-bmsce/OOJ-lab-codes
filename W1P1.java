72065581a248217d303b8bd1a8f5a6e3c7161d9a
sharan-s-pai <71424591+sharan-pai-bmsce@users.noreply.github.com>
2020-10-09 20:39:31 +0530

    Lab program - 1(extra questions)

---
+++ W1P1.java
@@ -0,0 +1,97 @@
+import java.util.Scanner;
+
+public class W1P1 {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//Lab Program - 1 (Extra Question - 1)
+		
+			// int num = input.nextInt();
+			// int value,sum1=0,sum2=0;
+			// for(int i=0;i<num;i++) {
+			// 	value = input.nextInt();
+			// 	if(i%2==0) {
+			// 		sum1+=value;
+			// 	}else {
+			// 		sum2+=value;
+			// 	}
+			// 	System.out.println("a["+i+"] = "+value);
+			// }
+			// 	System.out.println("The Sum of Even numbers: "+sum1);
+			//    System.out.println("The Sum of Odd numbers: "+sum2);
+
+		//Lab Program - 1 (Extra Question - 2)
+		
+			// int num = input.nextInt();
+			// int value,pos=0,neg=0,zer=0;
+			// for(int i=0;i<num;i++) {
+			// 	value = input.nextInt();
+			// 	if(value>0)
+			// 		pos++;
+			// 	if(value<0)
+			// 		neg++;
+			// 	if(value==0)
+			// 		zer++;
+			// }
+			// System.out.println("No. of positive integer: "+pos);
+			// System.out.println("No. of negative integer: "+neg);
+			// System.out.println("No. of zeros: "+zer);
+		
+		//Lab Program - 1 (Extra Question - 3)
+		
+			// double[] rate = new double[5];
+			// int[] quantity = new int[5];
+			// for(int i=0;i<5;i++) {
+			// 	rate[i] = input.nextDouble();
+			// 	quantity[i] = input.nextInt();
+			// }
+			// double subtotal=0;
+			// for(int i=0;i<5;i++) {
+			// 	subtotal+=(rate[i]*quantity[i]);
+			// }
+			// System.out.println("Sub-total: "+subtotal);
+			// if(subtotal>=10000) {
+			// 	subtotal-=(0.05*subtotal);
+			// }else if(subtotal>=7500) {
+			// 	subtotal-=(0.03*subtotal);
+			// }else if(subtotal>=5000) {
+			// 	subtotal-=(0.02*subtotal);
+			// }
+			// System.out.println("Total: "+subtotal);
+		
+		//Lab Program - 1 (Extra Question - 4)
+		
+			// int num = input.nextInt();
+			// int a[] = new int[num];
+			// int b[] = new int[num];
+			// int value,j=0,k=0;
+			// for(int i=0;i<num;i++) {
+			// 	value = input.nextInt();
+			// 	if(value%2==0) {
+			// 		a[j]=value;
+			// 		j++;
+			// 	}else {
+			// 		b[k]=value;
+			// 		k++;
+			// 	}
+			// }
+			// int sum=0;
+			// int small=a[0];
+			// int big = a[0];
+			// for(int i=0;i<j;i++) {
+			// 	sum+=a[i];
+			// 	if(small>a[i]) {
+			// 		small = a[i];
+			// 	}
+			// 	if(big<a[i]) {
+			// 		big = a[i];
+			// 	}
+			// }
+			// System.out.println("Sum of all integers in C "+sum);
+			// System.out.println("Average of all integers in C "+(double)sum/k);
+			// System.out.println("Smallest integer in C "+small);
+			// System.out.println("Largest integer in C "+big);
+	}
+
+}
