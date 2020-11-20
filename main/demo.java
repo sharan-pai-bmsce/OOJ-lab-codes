aafaea1e582f963cceeed019dec360f5e04fc974
Sharan Pai <sharans.cs19@bmsce.ac.in>
2020-11-20 13:25:21 +0530

    demo program for packages

---
+++ main/demo.java
@@ -0,0 +1,10 @@
+import main.*;
+
+class demo{
+    public static void main(String[] args){
+        Main m1 = new Main();
+        m1.x = 6;
+        System.out.println(m1.x);
+        m1.display();
+    }
+}
---
+++ main/main/Main.java
@@ -0,0 +1,8 @@
+package main;
+
+public class Main{
+    public int x=4;
+    public void display(){
+        System.out.println("this number is squared "+x*x);
+    }
+}
