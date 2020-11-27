0ab8f17741811fb2a0b0f7f83294ddd51c730506
Sharan Pai <sharans.cs19@bmsce.ac.in>
2020-11-27 12:18:32 +0530

    Lab-program-8

---
+++ Lab Program-8/lab8.java
@@ -0,0 +1,49 @@
+class ageException extends Exception{
+    int detail;
+    ageException(int a){
+        detail = a;
+    }
+    public String toString(){
+        return "Exception :"+detail+" the enterred age does not match the category";
+    }
+}
+class Father{
+    int age;
+    Father(int age) throws ageException{
+        this.age = age;
+        if(this.age<=0){
+            throw new ageException(this.age);
+        }
+    }
+    void display(){
+        System.out.println("Father's age:"+this.age);
+    }
+}
+
+class Son extends Father{
+    Father f;
+    Son(int age,Father f) throws ageException{
+        super(age);
+        this.f = f;
+        if(this.age>=this.f.age){
+            //System.out.println(f.age);
+            throw new ageException(this.age);
+        }
+    }
+    void display(){
+        this.f.display();
+        System.out.println("Son's age:"+this.age);
+    }
+}
+
+public class lab8{
+    public static void main(String[] args){
+        try{
+            Father f = new Father(32);
+            Son s = new Son(34,f);
+            s.display();
+        }catch(Exception e){
+            System.out.println(e);
+        }
+    }
+}
