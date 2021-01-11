00074452921d6922b807ccdba1de85d7c7c51706
sharan-s-pai <sharanpai2001@gmail.com>
2021-01-11 12:35:21 +0530

    Lab AWT extra Programs

---
+++ New AWT Program/ExtraProgram.java
@@ -0,0 +1,167 @@
+
+import java.applet.Applet;
+import java.awt.Color;
+import java.awt.Frame;
+import java.awt.Graphics;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+/*
+ * <applet code="ExtraProgram" width="1200" height="600"></applet>
+ * 
+ */
+// Lab Program-2
+public class ExtraProgram extends Applet implements MouseListener,MouseMotionListener {
+
+		String msg="";
+		int x=50;
+		int y=50;
+	public ExtraProgram(){
+		addMouseListener(this);
+		addMouseMotionListener(this);
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		x = e.getX();
+		y = e.getY();
+		msg="(x="+x+",y="+y+")";
+		setForeground(Color.RED);
+		repaint();
+	}
+	public void init() {
+		setBackground(Color.BLACK);
+	}
+	public void start() {
+		msg="Started";
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		msg="Mouse Clicked "+e.getClickCount()+" times";
+		setForeground(Color.ORANGE);
+		repaint();
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		x = e.getX();
+		y = e.getY();
+		msg = "Pressed At "+x+" "+y;
+		setForeground(Color.BLUE);
+		repaint();
+		
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		x = e.getX();
+		y = e.getY();
+		msg = "Released At "+x+" "+y;
+		setForeground(Color.CYAN);
+		repaint();
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		x=100;
+		y=100;
+		msg="Mouse Enterred";
+		setForeground(Color.YELLOW);
+		repaint();
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		x=100;
+		y=100;
+		setForeground(Color.GREEN);
+		msg="Mouse Exited";
+		repaint();
+	}
+	public void paint(Graphics g) {
+		g.drawString(msg, x, y);
+	}
+}
+
+//Lab Program-1
+
+/*public class ExtraProgram extends Frame implements MouseListener,MouseMotionListener{
+	String msg = "";
+	int x = 0,y = 0;
+	public ExtraProgram(){
+		addMouseListener(this);
+		addMouseMotionListener(this);
+		addWindowListener(new MyWindow());
+	}
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		msg="mouse clicked "+e.getClickCount();
+		repaint();
+	}
+	
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		x = getWidth()/2;
+		y = getHeight()/2;
+		msg = "mouse entered";
+		repaint();
+	}
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		x = 100;
+		y = 100;
+		msg = "mouse exited";
+		repaint();
+	}
+	@Override
+	public void mousePressed(MouseEvent e) {
+		x = e.getX();
+		y = e.getY();
+		msg = "Clicked";
+		repaint();
+	}
+	@Override
+	public void mouseDragged(MouseEvent e) {
+		// TODO Auto-generated method stub
+		x = e.getX();
+		y = e.getY();
+		msg = "x = "+x+"y = "+y;
+		repaint();
+	}
+	@Override
+	public void paint(Graphics g) {
+		super.paint(g);
+		g.drawString(msg,x,y);
+	}
+	public static void main(String[] args) {
+		Sample s = new Sample();
+		s.setSize(300, 300);
+		s.setVisible(true);
+	}
+	@Override
+	public void mouseMoved(MouseEvent e) {
+		
+	}
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		
+	}
+	
+}
+
+class MyWindow extends WindowAdapter{
+	@Override
+	public void windowClosing(WindowEvent e) {
+		super.windowClosing(e);
+		System.exit(0);
+	}
+}*/
